9f9eab740c4a9b22780e0bfda12a4822114a7b95
Rubén Palomo Fontán <99551384+RubenPalomo@users.noreply.github.com>
2022-09-18 14:56:28 +0200

    Add files via upload

--- 42_libft-master/ft_strnstr.c
+++ 42_libft-master/ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: rpalomo- <rpalomo-@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/25 19:58:51 by rpalomo-          #+#    #+#             */
-/*   Updated: 2022/08/26 19:35:57 by rpalomo-         ###   ########.fr       */
+/*   Updated: 2022/09/18 14:54:04 by rpalomo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 	size_t	i;
 	size_t	j;
 
-	if (!needle[0])
+	if (!needle[0] || *needle == '\0')
 		return ((char *)haystack);
 	i = 0;
 	while (haystack[i] && i < len)
